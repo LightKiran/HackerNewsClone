38d81d8dc136e4866d433fcb3a2adfc1f07fa3a8
LightKiran <kiranduwal@gmail.com>
2019-08-01 15:02:18 +0545

    simple random user api added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './hack/Header';
+import './hack/Mine.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  state = {
+    loading: true,
+    person: null
+  };
+
+  // best life cycle method if we are importing data from api
+  async componentDidMount(){
+    const url = "https://randomuser.me/api/";
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    this.setState({person: data.results[0], loading : false});
+  }
+
+  render(){
+  
+    return (
+     
+          <div>
+          <div className="container">
+            <Header />
+            <div>
+           {this.state.loading || !this.state.person ? (
+            <div>loading...</div>
+            ) : (
+            <div>{this.state.person.name.first}</div>
+            ) }
+         </div>
+          </div>
+          
+         
+         </div>
+
+        );
+
+  }
+
 }
+
+
+
 
 export default App;
---
+++ src/hack/Header.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+//import './Mine.css';
+
+
+function Header() {
+    return (
+      <div className="header">
+        
+        Hacker News
+        new | past | comments | ask | show | jobs | submit
+        <div id = "login">
+        login
+        </div>
+        
+      </div>
+    );
+  }
+
+export default Header;
