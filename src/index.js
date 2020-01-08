7d12d9bd119288ea0eaa4fcbf2821261c0fdde36
LightKiran <kiranduwal@gmail.com>
2020-01-08 19:42:24 +0545

    set global styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-import App from './App';
+import App from 'components/App';
+import setGlobalStyles from 'styles/globals';
+// import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+setGlobalStyles();
+
+// const renderApp = () => {
+    ReactDOM.render(
+    //  <GlobalStyles />,   
+     <App />, 
+    document.getElementById('root'),
+    );
+// };
+
+// renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// registerServiceWorker();
+
+serviceWorker.register();
