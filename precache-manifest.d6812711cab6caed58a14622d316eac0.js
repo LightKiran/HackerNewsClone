f74c222f8cdfb445f96668b082e8b408eaa6e106
LightKiran <kiranduwal@gmail.com>
2020-01-09 22:30:39 +0545

    Updates

---
+++ precache-manifest.d6812711cab6caed58a14622d316eac0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13caa1e296fa6a99d800",
+    "url": "/hacker_news_clone/static/js/runtime~main.13caa1e2.js"
+  },
+  {
+    "revision": "b46bd520102b40c290c3",
+    "url": "/hacker_news_clone/static/js/main.b46bd520.chunk.js"
+  },
+  {
+    "revision": "6e44df1f382c0184b435",
+    "url": "/hacker_news_clone/static/js/1.6e44df1f.chunk.js"
+  },
+  {
+    "revision": "5d7da0a657d42c865a8850dbed82c851",
+    "url": "/hacker_news_clone/index.html"
+  }
+];
